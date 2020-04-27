a682c59edddf61be92823fc95ca944e8f2e664be
Constanza <cotycirion94@gmail.com>
2020-04-26 21:25:17 -0300

    resuelvo objetivos 1 y 2 (ajustar tamaño de la barra y visibilidad de botones)

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 new Vue({
     el:'#app',
     data: {
-    
+        playerHealth: 100,
+        monsterHealth: 100,
+        gameIsRunning: false    
     },
     methods: {
-        
+        startGame: function() {
+            this.gameIsRunning = true;
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+        },
+        attack: function() {
+
+        },
+        specialAttack: function() {
+
+        }, 
+        heal: function() {
+
+        },
+        giveUp: function() {
+            this.gameIsRunning = false;
+        }         
     }
     });
