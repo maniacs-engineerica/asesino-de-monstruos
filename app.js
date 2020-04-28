2385d2fc267d1c5ab838086150f1be895a2c6a19
Matias Cohen <matiasc@engineerica.com>
2020-04-28 14:05:18 -0300

    Objectivo 4 y log de eventos

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ new Vue({
     data: {
         playerHealth: 100,
         monsterHealth: 100,
-        gameIsRunning: false    
+        gameIsRunning: false,
+        turns: []   
     },
     methods: {
         startGame: function() {
@@ -12,13 +13,22 @@ new Vue({
             this.monsterHealth = 100;
         },
         attack: function() {
-
+            this.turns.unshift({
+                isPlayer: true,
+                text: `El jugador golpea al monstruo ${5}%`
+            })
         },
         specialAttack: function() {
-
+            let damage = this.calculateDamage(10, 20)
+            this.monsterHealth -= damage;
+            if (this.checkWin()){
+                return;
+            }
+            this.monsterAttacks();
         }, 
         heal: function() {
-
+            this.playerHealth = Math.min(100, playerHealth+10)
+            this.monsterAttacks()
         },
         giveUp: function() {
             this.gameIsRunning = false;
